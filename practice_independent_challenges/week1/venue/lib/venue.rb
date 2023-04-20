886786866efdf127f95314ffa7eb7aae356c4562
Weston Schutt <wtschutt@gmail.com>
2023-04-20 09:19:49 -0600

    Add completed Venue independent challenge

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,19 @@
-class Venue
+class Venue 
+    attr_reader :name, :capacity, :patrons
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def add_patron(name)
+        @patrons.push(name.capitalize)
+    end
+
+    def yell_at_patrons
+        @patrons.map! do |patron|
+            patron.upcase
+        end
+    end
 end
