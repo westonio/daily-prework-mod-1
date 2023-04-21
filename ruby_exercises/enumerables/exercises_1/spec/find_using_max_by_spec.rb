5e06967b0488ef176f1833f536435cd63a60993b
Weston Schutt <wtschutt@gmail.com>
2023-04-21 16:19:19 -0600

    Add completed find/detect/max_by enumerable exercises

--- ruby_exercises/enumerables/exercises_1/spec/find_using_max_by_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/find_using_max_by_spec.rb
@@ -5,69 +5,80 @@ RSpec.describe 'max_by' do
 
     found_word = words.max_by do |word|
       word.length
-    end
+    end 
 
     expect(found_word).to eq("eggplant")
   end
 
   # This one is missing the block inside the loop.
-  xit 'shortest word' do
+  it 'shortest word' do
     words = ["apple", "banana", "cherry", "date", "eggplant"]
 
     found_word = words.max_by do |word|
-      # write code here
+      -word.length #the negative operator before word.length will result in the shortest
     end
 
     expect(found_word).to eq("date")
   end
 
   # This one is missing the entire loop
-  xit 'array with the most items' do
+  it 'array with the most items' do
     arrays = [[:a, :b, :c], [1, 2, 3, 4, 5], ["zoo", :things, :stuff]]
 
-    # write code here
+    biggest_array = arrays.max_by do |array|
+      array.length
+    end
 
     expect(biggest_array).to eq([1,2,3,4,5])
   end
 
   # You're on your own on this one.
-  xit 'array with fewest items' do
+  it 'array with fewest items' do
     arrays = [[:a, :b, :c], [1, 2, 3, 4, 5], ["zoo", :things, :stuff]]
 
-    # write code here
+    smallest_array = arrays.max_by do |array|
+      -array.length
+    end
 
     expect(smallest_array).to eq([:a, :b, :c])
   end
 
-  xit 'biggest number' do
+  it 'biggest number' do
     numbers = [1, 10, 100, 1000, 10000, 1000000]
 
-    # write code here
+    found = numbers.max_by do |number|
+      number
+    end
 
     expect(found).to eq(1000000)
   end
 
-  xit 'smallest number' do
+  it 'smallest number' do
     numbers = [1, 10, 100, 1000, 10000, 1000000]
 
-    # write code here
+    found = numbers.max_by do |number|
+      -number
+    end
 
     expect(found).to eq(1)
   end
 
-  xit 'most programmers' do
+  it 'most programmers' do
     programmers = {ruby: ["katrina", "sandi", "jim", "aaron", "desi"], java: ["abby", "jon", "susan"]}
 
-    # write code here
+    most_programmers = programmers.max_by do |language|
+      language[1].length
+    end
 
     expect(most_programmers.first).to eq(:ruby)
   end
 
-  xit 'fewest programmers' do
+  it 'fewest programmers' do
     programmers = {ruby: ["katrina", "sandi", "jim", "aaron", "desi"], java: ["abby", "jon", "susan"]}
-
-    # write code here
-
+    fewest_programmers = programmers.max_by do |language|
+      -language[1].length
+    end
+    
     expect(fewest_programmers.first).to eq(:java)
   end
 end
