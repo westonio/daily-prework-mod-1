48d7d08581d093baf67f3ddbd9cb57ed6dc18ee1
Weston Schutt <wtschutt@gmail.com>
2023-04-27 12:24:50 -0600

    Add completed select iteration exercise

--- ruby_exercises/iteration/exercises/spec/select_pattern_spec.rb
+++ ruby_exercises/iteration/exercises/spec/select_pattern_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'select pattern' do
   it 'test 1' do
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     evens = []
-    numbers.each do |number|
+    numbers.select do |number|
       evens << number if number.even?
     end
     expect(evens).to eq([2, 4, 6, 8, 10])
@@ -18,7 +18,7 @@ RSpec.describe 'select pattern' do
       five: 5,
     }
     evens = {}
-    numbers.each do |name, number|
+    numbers.select do |name, number|
       if number.even?
         evens[name] = number
       end
@@ -31,17 +31,17 @@ RSpec.describe 'select pattern' do
     expect(evens).to eq(expected)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
     greater_than_four = []
-    rainbow.each do |color|
-      #Your Code Here
+    rainbow.select do |color|
+      greater_than_four << color if color.length > 4
     end
 
     expect(greater_than_four).to eq(["orange", "yellow", "green", "indigo", "violet"])
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     rainbow = {
       first: "red",
       second: "orange",
@@ -52,8 +52,8 @@ RSpec.describe 'select pattern' do
       seventh: "violet"
     }
     greater_than_four = {}
-    rainbow.each do |position, color|
-      #Your Code Here
+    rainbow.select do |position, color|
+      greater_than_four[position] = color if color.length > 4
     end
 
     expected = {
@@ -63,18 +63,20 @@ RSpec.describe 'select pattern' do
       sixth: "indigo",
       seventh: "violet"
     }
-    expected(greater_than_four).to eq(expected)
+    expect(greater_than_four).to eq(expected)
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     furniture = ["dining table", "bed", "coffee table", "deck chairs"]
     words_with_c = []
-    #Your Code Here
+    furniture.select do |item|
+      words_with_c << item if item.include?("c")
+    end
 
     expect(words_with_c).to eq(["coffee table", "deck chairs"])
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     furniture = {
       dining_room: "dining table",
       bedroom: "bed",
@@ -82,7 +84,9 @@ RSpec.describe 'select pattern' do
       patio: "deck chairs"
     }
     words_with_c = {}
-    #Your Code Here
+    furniture.select do |room,item|
+      words_with_c[room] = item if item.include?("c")
+    end
 
     expected = {
       living_room: "coffee table",
@@ -91,20 +95,26 @@ RSpec.describe 'select pattern' do
     expect(words_with_c).to eq(expected)
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     meals = ["chips and salsa", "chicken alfredo", "banana pudding"]
-    #Your Code Here
+    two_words = []
+    meals.select do |meal|
+      two_words << meal if meal.count(' ') == 1
+    end
 
     expect(two_words).to eq(["chicken alfredo", "banana pudding"])
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     meal = {
       appetizer: "chips and salsa",
       entre: "chicken alfredo",
       dessert: "banana pudding"
     }
-    #Your Code Here
+    two_words = {}
+    meal.select do |type,food|
+      two_words[type] = food if food.count(' ') == 1
+    end
 
     expected = {
       entre: "chicken alfredo",
@@ -114,14 +124,17 @@ RSpec.describe 'select pattern' do
   end
 
 
-  xit 'test 9' do
+  it 'test 9' do
     prices = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
-    #Your Code Here
+    floats = []
+    prices.select do |number|
+      floats << number if number.is_a?(Float)
+    end
 
     expect(floats).to eq([1.4, 3.5, 4.9, 9.1, 8.0])
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     items = {
       tv: 3,
       toaster: 1.4,
@@ -131,7 +144,10 @@ RSpec.describe 'select pattern' do
       sack_o_potatoes: 9.1,
       tonka_truck: 8.0
     }
-    #Your Code Here
+    floats = {}
+    items.select do |item,price|
+      floats[item] = price if price.is_a?(Float)
+    end
 
     expected = {
       toaster: 1.4,
