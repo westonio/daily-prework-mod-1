011a22f34a1f29fa0ca7f8b2eb229113e34ebb61
Weston Schutt <wtschutt@gmail.com>
2023-04-28 13:23:07 -0600

    Add completed .all? iteration exercise

--- ruby_exercises/iteration/exercises/spec/all_pattern_spec.rb
+++ ruby_exercises/iteration/exercises/spec/all_pattern_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe 'all pattern' do
   it 'test 1' do
     account_balances = [0, 0, 0, 0, 0, 0, 0]
-    all_zeros = true
-    account_balances.each do |balance|
-      all_zeros = false unless balance.zero?
+    all_zeros = false
+    account_balances.all? do |balance|
+      all_zeros = balance.zero?
     end
     expect(all_zeros).to be true
   end
@@ -15,25 +15,23 @@ RSpec.describe 'all pattern' do
       retirement_401k: 0,
       retirement_ira: 0,
     }
-    all_zeros = true
-    account_balances.each do |account, balance|
-      if !balance.zero?
-        all_zeros = false
-      end
+    all_zeros = false
+    account_balances.all? do |account, balance|
+      all_zeros = balance.zero?
     end
     expect(all_zeros).to be true
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     words = ["love", "hate", "fire", "bird", "call"]
-    all_four_letters = true
-    words.each do |word|
-      # Your Code Here
+    all_four_letters = false
+    words.all? do |word|
+      all_four_letters = word.length == 4
     end
     expect(all_four_letters).to be true
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     words = {
       one: "love",
       two: "hate",
@@ -42,40 +40,46 @@ RSpec.describe 'all pattern' do
       five: "call"
     }
     all_four_letters = true
-    words.each do |position, word|
-      # Your Code Here
+    words.all? do |position, word|
+      all_four_letters = word.length == 4
     end
     expect(all_four_letters).to be true
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     statuses = [:busy, :busy, :busy]
     all_busy = true
-    # Your Code Here
-
+    statuses.all? do |status|
+      all_busy = status == :busy
+    end
     expect(all_busy).to be true
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     friend_status = {
       "Megan" => :busy,
       "Sarah" => :busy,
       "Duncan" => :busy,
     }
-    all_busy = true
-    # Your Code Here
+    all_busy = false
+    friend_status.all? do |friend,status|
+      all_busy = status == :busy
+    end
 
     expect(all_busy).to be true
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     zip_codes = [94381, 831, 50009, 36232, 8992, 89999, 11110]
-    # Your Code Here
+    all_five_digits = true
+    zip_codes.all? do |zip_code|
+      all_five_digits = zip_code.to_s.length == 5
+    end
 
     expect(all_five_digits).to be false
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     zip_codes = {
       "Megan" => 94381,
       "Sarah" => 831,
@@ -85,26 +89,33 @@ RSpec.describe 'all pattern' do
       "Cameron" => 89999,
       "Joshua" => 11110
     }
-    # Your Code Here
+    all_five_digits = true
+    zip_codes.all? do |zip_code|
+      all_five_digits = zip_code.to_s.length == 5
+    end
 
     expect(all_five_digits).to be false
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     snacks = ["GARLIC PLANTAINS", "SNICKERDOODLES", "Pretzels"]
-    # Your Code Here
-
+    all_caps = true
+    snacks.all? do |snack|
+      all_caps = snack == snack.upcase
+    end
     expect(all_caps).to be false
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     snacks = {
       savory: "GARLIC PLANTAINS",
       sweet: "SNICKERDOODLES",
       salty: "Pretzels"
     }
-    # Your Code Here
-
+    all_caps = true
+    snacks.all? do |type,snack|
+      all_caps = snack.upcase == snack
+    end
     expect(all_caps).to be false
   end
 end
