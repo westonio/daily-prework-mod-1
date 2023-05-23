849c0fc843aa4afd76dfbce6c2e86b056cecf41e
Weston Schutt <wtschutt@gmail.com>
2023-05-23 15:50:16 -0600

    Add nested colelctions exercises (incomplete)

--- lessons/nested_collections/Examples/hash_within_hash.rb
+++ lessons/nested_collections/Examples/hash_within_hash.rb
@@ -3,7 +3,16 @@ pets = {:dog => {:name => "Chance", :weight => "45 pounds"},
        }
 
 # 1. what is pets.count
+2
+p pets.count
 # 2. what is pets.keys
+[:dog, :cat]
+p pets.keys
 # 3. what is pets.values
+[{:name => "Chance", :weight => "45 pounds"},{:name => "Sassy", :weight => "15 pounds"}]
+p pets.values
 # 4. how can I access the element "15 pounds"
+p pets[:cat][:weight]
 # 5. how can I add :age => 3 to the value of the key :dog
+pets[:dog][:age] = 3
+p pets
--- lessons/nested_collections/Examples/hashes_within_arrays.rb
+++ lessons/nested_collections/Examples/hashes_within_arrays.rb
@@ -1,6 +1,8 @@
 food_feelings = [{:pizza => "tasty"}, {:calzone => "also tasty"}]
 
-# 1. what is food_feelings.count
-# 2. what is food_feelings.first.count
+p food_feelings.count 
+p food_feelings.first.count
 # 3. how can I access the value "also tasty"
+p food_feelings[1]
 # 4. how can I change the value “also tasty” to “super delicious”
+
