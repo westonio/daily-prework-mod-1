7ef3efb3e37a939be66eb9fcd6f2e48557b61667
Weston Schutt <wtschutt@gmail.com>
2023-05-26 09:52:21 -0600

    Add orders Module exercise

--- lessons/modules/orders/lib/amazon_order.rb
+++ lessons/modules/orders/lib/amazon_order.rb
@@ -1,14 +1,17 @@
+require './lib/orderable'
+
 # ./amazon_order.rb
 class AmazonOrder
-  def confirmation(thing)
-    "You got #{thing}."
-  end
+  include Orderable
+  # def confirmation(thing)
+  #   "You got #{thing}."
+  # end
 
   def delivery
     "Your order will arrive in 2 business days."
   end
 
-  def review
-    "Please rate your order within 30 days."
-  end
+  # def review
+  #   "Please rate your order within 30 days."
+  # end
 end
---
+++ lessons/modules/orders/lib/orderable.rb
@@ -0,0 +1,9 @@
+module Orderable
+  def confirmation(thing)
+      "You got #{thing}."
+  end
+
+  def review
+      "Please rate your order within 30 days."
+  end
+end
