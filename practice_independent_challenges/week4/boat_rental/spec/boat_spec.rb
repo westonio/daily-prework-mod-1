3ca4bad8fea122fb9f31986ce1bdabafce90aca3
Weston Schutt <wtschutt@gmail.com>
2023-06-02 15:23:12 -0600

    Add iteration 1 Boat class tests

---
+++ practice_independent_challenges/week4/boat_rental/spec/boat_spec.rb
@@ -0,0 +1,32 @@
+require './lib/boat'
+
+RSpec.describe Boat do
+  before(:each) do
+    @kayak = Boat.new(:kayak, 20)
+  end
+
+  it 'exists' do
+    expect(@kayak.exists?).to eq true
+    expect(@kayak).to be_a Boat
+  end
+
+  it 'has a boat type' do
+    expect(@kayak.type).to eq :kayak
+  end
+
+  it 'has hourly rate' do
+    expect(@kayak.price_per_hour).to eq 20
+  end
+
+  it 'starts with zero hours reted by default' do
+    expect(@kayak.hours_rented).to eq 0
+  end
+
+  it 'can have rented hours added' do
+    @kayak.add_hour
+    @kayak.add_hour
+    @kayak.add_hour
+
+    expect(@kayak.hours_rented).to eq 3
+  end
+end
