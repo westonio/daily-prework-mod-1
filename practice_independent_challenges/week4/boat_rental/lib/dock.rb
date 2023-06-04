fb0f81252deb945ba68b249aab58d04fa5852bb5
Weston Schutt <wtschutt@gmail.com>
2023-06-04 09:59:38 -0600

    Complete iteration 4 tests and methods

--- practice_independent_challenges/week4/boat_rental/lib/dock.rb
+++ practice_independent_challenges/week4/boat_rental/lib/dock.rb
@@ -1,26 +1,50 @@
 class Dock
   attr_reader :name, 
               :max_rental_time,
-              :rental_log
+              :rental_log,
+              :boats_currently_rented,
+              :revenue
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
     @rental_log = {}
+    @boats_currently_rented = []
+    @revenue = 0
   end
 
   def rent(boat, renter)
-    rental_log[boat] = renter
+    if @boats_currently_rented.include?(boat)
+      "Already Rented - Please wait for Return"
+    else
+      rental_log[boat] = renter
+      @boats_currently_rented << boat
+    end
+  end
+
+  def return(boat)
+    if @boats_currently_rented.include?(boat)
+      @boats_currently_rented.delete(boat)
+    else
+      "This boat must be rented first in order to be returned"
+    end
   end
 
   def charge(boat, renter)
     hours = get_hours(boat)
     amount = (hours * boat.price_per_hour)
+    @revenue += amount
     {:card_number => renter.credit_card_number, :amount => amount}
   end
 
   def get_hours(boat)
     boat.hours_rented < @max_rental_time ? boat.hours_rented : @max_rental_time
+  end
+
+  def log_hour
+    @boats_currently_rented.each do |boat|
+      boat.add_hour
+    end
   end
 
 end
--- practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
@@ -56,4 +56,50 @@ RSpec.describe Dock do
     expect(@kayak_1.hours_rented).to eq(4)
     expect(@dock.charge(@kayak_1,@patrick)).to eq(expected_charge)
   end
+
+  it 'tracks rented boats' do
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@sup_1, @eugene)
+
+    expect(@dock.boats_currently_rented).to eq([@kayak_1, @sup_1])
+  end
+
+  it 'can only rent boat if not already rented' do
+    @dock.rent(@kayak_1, @patrick)
+
+    expect(@dock.rent(@kayak_1, @eugene)).to eq("Already Rented - Please wait for Return")
+  end
+
+  it 'can return boats' do
+    expect(@dock.return(@kayak_1)).to eq("This boat must be rented first in order to be returned")
+    @dock.rent(@kayak_1, @patrick)
+    expect(@dock.boats_currently_rented).to eq([@kayak_1])
+    
+    @dock.return(@kayak_1)
+    expect(@dock.boats_currently_rented).to eq([])
+  end
+
+  it 'can log an hour for all boats currently rented' do
+    @dock.rent(@kayak_1, @patrick)
+    expect(@kayak_1.hours_rented).to eq(0)
+
+    @dock.log_hour
+    expect(@kayak_1.hours_rented).to eq(1)
+    @dock.rent(@sup_1, @eugene)
+
+    @dock.log_hour
+    expect(@kayak_1.hours_rented).to eq(2)
+    expect(@sup_1.hours_rented).to eq(1)
+  end
+
+  it 'can track the revenue for all charges' do
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@sup_1, @eugene)
+
+    4.times {@dock.log_hour}
+    @dock.charge(@kayak_1, @patrick) # $60 for max 3 hour charge.
+    @dock.charge(@sup_1, @eugene) # $45 for max 3 hour charge.
+
+    expect(@dock.revenue).to eq(105)
+  end
 end
