2249eb7318b2bb4131622b2dbbb3e4da860c1539
Weston Schutt <wtschutt@gmail.com>
2023-06-11 09:43:19 -0600

    Test & Feature: Add Artist class methods and tests

---
+++ practice_independent_challenges/week5/museo/lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+  attr_reader :id, 
+              :name, 
+              :born, 
+              :died,
+              :country
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
+  end
+
+  def age_at_death
+    died.to_i - born.to_i
+  end
+end
